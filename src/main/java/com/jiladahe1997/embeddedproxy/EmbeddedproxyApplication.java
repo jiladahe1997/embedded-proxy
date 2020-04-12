de65f58efeda79507bb0f59b7652c6cdc07ed066
Ren Mingrui <972931182@qq.com>
2020-04-12 22:11:02 +0800

    keil下载和分片上传任务开发完成，准备发布到test分支

--- src/main/java/com/jiladahe1997/embeddedproxy/EmbeddedproxyApplication.java
+++ src/main/java/com/jiladahe1997/embeddedproxy/EmbeddedproxyApplication.java
@@ -1,13 +1,16 @@
 package com.jiladahe1997.embeddedproxy;
 
+import com.jiladahe1997.embeddedproxy.scheduling.JobKeil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.io.IOException;
+
 @SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
 public class EmbeddedproxyApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		SpringApplication.run(EmbeddedproxyApplication.class, args);
 	}
 
---
+++ src/main/java/com/jiladahe1997/embeddedproxy/models/scheduling/MultipartUploadBase.java
@@ -0,0 +1,5 @@
+package com.jiladahe1997.embeddedproxy.models.scheduling;
+
+public class MultipartUploadBase {
+
+}
